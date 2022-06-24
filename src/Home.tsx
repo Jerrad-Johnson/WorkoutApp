77eb0b4536d6f118cb7204ecaef47bd3f04ef68a
Jerrad-Johnson <j_johnson21@mail.fhsu.edu>
2022-06-24 17:01:22 -0600

    login and session entry backend integration works.

--- src/Home.tsx
+++ src/Home.tsx
@@ -9,7 +9,8 @@ function Home(){
     const [defaultRepCountState, setDefaultRepCountState] = useState<number>(5);
     const [serverResponseState, setServerResponseState] = useState<string>("Awaiting response...");
 
-    let date: string = todaysDateForHTMLCalendar()
+    let date: string = todaysDateForHTMLCalendar();
+    TestingLogin();
 
     return(
       <div className={""}>
@@ -122,19 +123,49 @@ function todaysDateForHTMLCalendar(){
     return (`${yyyy}-${MM}-${DD}`);
 }
 
-function TestingCors({serverResponseState}: {serverResponseState: string}){
-    let x: object = {
-        'lol':'idk'
-    }
+function TestingLogin() {
+    let entry: object = {
+        "password": "abc",
+        "username": "elseif",
+    };
 
-    fetch("http://localhost:80/php/test.php", {
+    fetch("http://localhost:80/php/login.php", {
         method: 'POST',
         mode: 'cors',
-        body: JSON.stringify(x),
+        body: JSON.stringify(entry),
+        credentials: "include",
+        headers: {
+            'Content-Type': 'application/json' }
+    }).then(response => response.json())
+        .then(data => cc(data));
+
+    return (<></>)
+}
+
+function TestingCors({serverResponseState}: {serverResponseState: string}){
+    let entries: object[] = [{
+        "date": "2022-02-02",
+        "title": "Upper Body",
+        "exercise": "Chest Press",
+        "weightLifted": [150, 150, 150, 150],
+        "reps": [10, 10, 10, 10],
+    },{
+        "date": "2022-02-02",
+        "title": "Upper Body",
+        "exercise": "Bicep Curl",
+        "weightLifted": [30, 30, 30],
+        "reps": [5, 5, 5],
+    }];
+
+    fetch("http://localhost:80/php/sessionentry.php", {
+        method: 'POST',
+        mode: 'cors',
+        body: JSON.stringify(entries),
+        credentials: "include",
         headers: {
         'Content-Type': 'application/json' }
     }).then(response => response.json())
-      .then(data => cc(JSON.parse(data)));
+      .then(data => cc(data));
 
     return (<></>)
 }
